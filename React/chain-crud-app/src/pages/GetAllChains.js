a807f14d36fb572bf9be86ca5e3fe853a4fddefa
vardhanghelani <vardhanghelani@gmail.com>
2025-06-19 12:30:52 +0530

    first

--- React/chain-crud-app/src/pages/GetAllChains.js
+++ React/chain-crud-app/src/pages/GetAllChains.js
@@ -7,7 +7,7 @@ function GetAllChains() {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const apiUrl = `${process.env.REACT_APP_API_URL}/Chains/`;
+        const apiUrl = `${process.env.REACT_APP_API_URL}/chains/`;
         ;
         
         const fetchData = async () => {
